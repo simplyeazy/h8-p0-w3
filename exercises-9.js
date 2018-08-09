e9ee61728f277b83ed6688cfc4f72296a722b0af
Lundy Orlando <simplyeazy@gmail.com>
2018-08-10 05:34:20 +0700

    First Commit

---
+++ exercises-9.js
@@ -0,0 +1,14 @@
+function cariMean(arr) {
+    let sum=0;
+    for (let index = 0; index <arr.length; index++) {
+        sum = sum + arr[index]
+    }
+    return Math.round(sum/arr.length);
+  }
+  
+  // TEST CASES
+  console.log(cariMean([1, 2, 3, 4, 5])); // 3
+  console.log(cariMean([3, 5, 7, 5, 3])); // 5
+  console.log(cariMean([6, 5, 4, 7, 3])); // 5
+  console.log(cariMean([1, 3, 3])); // 2
+  console.log(cariMean([7, 7, 7, 7, 7])); // 7
