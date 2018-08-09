7e94fb590c51a4cb7edb7645552c0fc971c9010d
Lundy Orlando <simplyeazy@gmail.com>
2018-08-10 05:34:38 +0700

    First Commit

--- exercises-5.js
+++ exercises-5.js
@@ -1,12 +1,12 @@
 function palindrome(kata) {
     let walik = "";
-    for (let index = kata.length-1; index >=0; index--) {
+    for (let index = kata.length - 1; index >= 0; index--) {
         walik = walik + kata[index];
     }
     // console.log(walik)
     // console.log(kata)
     if (walik === kata) {
-    return true;
+        return true;
     }
     return false;
 }
